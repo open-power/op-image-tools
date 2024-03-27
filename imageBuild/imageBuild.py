4f596e63ca57cca295e602c0b1eeb408f7ed9f54
Douglas Gilbert <dgilbert@us.ibm.com>
2024-03-27 12:38:30 -0500

    Modify behaviour of tarfile extract for higher security
    See https://jazz07.rchland.ibm.com:13443/jazz/resource/itemName/com.ibm.team.workitem.WorkItem/598771

--- imageBuild/imageBuild.py
+++ imageBuild/imageBuild.py
@@ -7,6 +7,7 @@ import subprocess
 import ast
 import shutil
 import tarfile
+import inspect
 
 def readConfigFile(configFile):
     with open(configFile, "r") as f:
@@ -369,7 +370,10 @@ def resolveFile(fpath, replacement_tags, overrides, binaries):
         # untar tar -C binaries -xzf binaries/sbe_images/odyssey_dd1_0/golden/golden_odyssey_nor_DD1.img.tar.gz
         tar = tarfile.open(newPath)
         # TODO might want to consider case where newPath is in an unwriteable location
-        tar.extractall(os.path.dirname(newPath))
+        if 'filter' in inspect.signature(tarfile.TarFile.extractall).parameters:
+            tar.extractall(os.path.dirname(newPath),filter="data")
+        else:
+            tar.extractall(os.path.dirname(newPath))
         newPath=newPath[:-len(tgzext)]
     print(f"INFO: Using {newPath}")
     return newPath
@@ -545,7 +549,12 @@ else:
     sbeToolsTar = os.path.join(sbeImageDir, sbeToolsTar)
 
 sbeTools = tarfile.open(sbeToolsTar)
-sbeTools.extractall(output)
+
+if 'filter' in inspect.signature(tarfile.TarFile.extractall).parameters:
+    sbeTools.extractall(output,filter="data")
+else:
+    sbeTools.extractall(output)
+
 sbeToolsDir = os.path.join(output,'sbe_tools')
 sbeImageTool = os.path.join(sbeToolsDir, 'imageTool.py')
 
@@ -842,7 +851,12 @@ if concatCopies > 1:
             print("INFO: Untar odyssey_sbe_debug_DD1.tar.gz")
             archive = tarfile.open(archSbeDebugTar)
             pathSbeDebugTar = os.path.dirname(os.path.abspath(archSbeDebugTar))
-            archive.extractall(pathSbeDebugTar)
+
+            if 'filter' in inspect.signature(tarfile.TarFile.extractall).parameters:
+                archive.extractall(pathSbeDebugTar,filter="data")
+            else:
+                archive.extractall(pathSbeDebugTar)
+
             archive.close()
 
             # Remove odyssey_sbe_debug_DD1.tar.gz
